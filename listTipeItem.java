a25f6a8d9bb8b4bbe013ddd67eb541a273a69ff9
Pausgunung <pausgunung@Gmail.com>
2024-11-19 09:16:35 +0700

    update

--- listTipeItem.java
+++ listTipeItem.java
@@ -37,6 +37,33 @@ package WaroengBleksmid;
                 new item("Silver Crossbow", 1,0,""),
                 new item("Diamond Crossbow", 1,0,""),
 
+                new item("Copper Sword", 1,0,"Bilahan pertama dari sang pandai besi, sederhana namun memiliki semangat untuk menorehkan sejarah baru."),
+                new item("Iron Sword", 1,0,"Sebilah penjaga yang kokoh, setia menemani setiap perjalanan ke medan perang."),
+                new item("Silver Sword", 1,0,"Pedang yang memantulkan cahaya malam, membawa harapan bagi yang melawan bayangan kelam."),
+                new item("Diamond Sword", 1,0,"Harta dari kedalaman bumi, melambangkan kemurnian dan kekuatan para pahlawan."),
+
+                new item("Copper Spear", 1,0,"Batang tajam yang bagaikan sinar mentari pagi, memancarkan kekuatan pemula."),
+                new item("Iron Spear", 1,0,"Tombak tak tergoyahkan, dengan kekuatan yang mampu menggetarkan hati lawan."),
+                new item("Silver Spear", 1,0,"Senjata yang membawa berkah bulan, menembus jiwa mereka yang tersembunyi dalam kegelapan."),
+                new item("Diamond Spear", 1,0,"Ujung tombak yang seperti serpihan bintang, tak terkalahkan oleh waktu."),
+
+                new item("Copper Axe", 1,0,"Pisau yang lahir dari hutan, sederhana namun mampu membuka jalan melalui rintangan."),
+                new item("Iron Axe", 1,0,"Kapak dengan suara nyaring, mengaum seperti serigala di tengah pertempuran."),
+                new item("Silver Axe", 1,0,"Kapak bercahaya, membawa kemurnian dalam setiap ayunan yang penuh makna."),
+                new item("Diamond Axe", 1,0,"Bilahan tak tertandingi, seperti gunung yang menghancurkan apapun yang menghalangi jalannya."),
+
+                new item("Copper Mace", 1,0,"Tangan kehancuran yang sederhana, tetapi cukup kuat untuk menundukkan mereka yang ceroboh."),
+                new item("Iron Mace", 1,0,"Sebuah palu dengan kekuatan yang bergema seperti lonceng perang."),
+                new item("Silver Mace", 1,0,"Pemukul yang bercahaya, membawa keadilan kepada mereka yang berani menantang takdir."),
+                new item("Diamond Mace", 1,0,"Kekuatan murni yang berkilau seperti matahari, menghantam dengan keagungan tiada tara."),
+
+                new item("Bow", 1,0,"Busur yang terbuat dari bisikan pepohonan, melontarkan anak panah seperti angin musim gugur."),
+                new item("Arrow", 1,0,"Panah tajam bagaikan mata elang, melesat ke tujuan dengan ketepatan sempurna."),
+                new item("Copper Crossbow", 1,0,"Alat perang yang terbuat dari tekad pemula, cepat dan andal dalam memburu musuh."),
+                new item("Iron Crossbow", 1,0,"Penembak jarak jauh dengan kekuatan yang tak goyah, melindungi dari serangan mendadak."),
+                new item("Silver Crossbow", 1,0,"Senjata dengan sihir tersembunyi, mengirimkan pesan malam kepada musuh yang bersembunyi."),
+                new item("Diamond Crossbow", 1,0,"Penguasa medan perang yang bercahaya, melesatkan anak panah bagaikan meteorit."),
+
                 new item("Staff", 1,0,""),
             };
     }
